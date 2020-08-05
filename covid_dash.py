e66bbe2c61f8038fd89a1dd76d9874fcefe888a6
pomkos <albeithome@gmail.com>
2020-08-05 17:34:29 +0000

    update port on test server

--- covid_dash.py
+++ covid_dash.py
@@ -252,4 +252,4 @@ def update_total_pos_case(state_value):
 #################
 
 if __name__ == '__main__':
-    app.run_server(debug=False,host="0.0.0.0",port=8050)
+    app.run_server(debug=False,host="0.0.0.0",port=8051)
